267a5371fab279a49e681ff03ee6efbd6835026d
Maria Gryglewska <maria.gryglewska51@gmail.com>
2021-04-06 14:55:15 +0200

    Added DriverComposite Functions

--- src/main/java/edu/kis/powp/jobs2d/drivers/composite/DriverComposite.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/composite/DriverComposite.java
@@ -2,29 +2,40 @@ package edu.kis.powp.jobs2d.drivers.composite;
 
 import edu.kis.powp.jobs2d.Job2dDriver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DriverComposite implements IDriverComposite {
+    private List<Job2dDriver> children = new ArrayList<>();
+
     @Override
     public void setPosition(int x, int y) {
-
+        for (Job2dDriver driver : children) {
+            driver.setPosition(x, y);
+        }
     }
 
     @Override
     public void operateTo(int x, int y) {
-
+        for (Job2dDriver driver : children) {
+            driver.operateTo(x, y);
+        }
     }
 
     @Override
     public void add(Job2dDriver item) {
-
+        children.add(item);
     }
 
     @Override
     public void remove(Job2dDriver item) {
-
+        children.remove(item);
     }
 
     @Override
     public Job2dDriver[] getChildren() {
-        return new Job2dDriver[0];
+        Job2dDriver[] driverChildren = new Job2dDriver[children.size()];
+        children.toArray(driverChildren);
+        return driverChildren;
     }
 }
