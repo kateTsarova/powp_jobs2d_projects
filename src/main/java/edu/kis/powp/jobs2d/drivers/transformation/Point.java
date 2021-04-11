791b199077e83c257015e581e5639464057b0ddb
kateryna <katerynatsarova@gmail.com>
2021-04-11 19:32:11 +0300

    created class TransformationDriver

--- src/main/java/edu/kis/powp/jobs2d/drivers/transformation/Point.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/transformation/Point.java
@@ -1,8 +1,8 @@
 package edu.kis.powp.jobs2d.drivers.transformation;
 
 public class Point {
-    int x;
-    int y;
+    public int x;
+    public int y;
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
