ab0619a297ec5c08e9c273ee0d0c8ee5a6a303c4
ProjectAntZ <luki111.1998@gmail.com>
2021-04-14 13:17:01 +0200

    Refractoring

--- src/test/java/edu/kis/powp/jobs2d/events/SelectLoadTriangleCommandOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectLoadTriangleCommandOptionListener.java
@@ -14,6 +14,8 @@ public class SelectLoadTriangleCommandOptionListener implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         ICompoundCommand command = new TriangleFigureComplexCommand().createCompoundCommand();
-        CommandsFeature.getDriverCommandManager().setCurrentCommand(command);
+
+        DriverCommandManager manager = CommandsFeature.getDriverCommandManager();
+        manager.setCurrentCommand(command);
     }
 }
