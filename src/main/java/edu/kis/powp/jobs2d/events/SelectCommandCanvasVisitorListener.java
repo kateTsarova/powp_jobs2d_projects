dd8c4a416b748c51fba30f40abc2346cf98552be
kateryna <katerynatsarova@gmail.com>
2021-04-23 20:39:37 +0300

    created class SelectCommandCanvasVisitorListener

---
+++ src/main/java/edu/kis/powp/jobs2d/events/SelectCommandCanvasVisitorListener.java
@@ -0,0 +1,42 @@
+package edu.kis.powp.jobs2d.events;
+
+import edu.kis.powp.jobs2d.command.DriverCommand;
+import edu.kis.powp.jobs2d.command.visitor.Canvas;
+import edu.kis.powp.jobs2d.command.visitor.CommandCanvasVisitor;
+import edu.kis.powp.jobs2d.drivers.DriverManager;
+import edu.kis.powp.jobs2d.features.CommandsFeature;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Logger;
+
+public class SelectCommandCanvasVisitorListener implements ActionListener {
+
+    private Logger logger = Logger.getLogger("global");
+    private DriverManager driverManager ;
+    private Canvas canvas;
+
+    public SelectCommandCanvasVisitorListener(DriverManager driverManager, Canvas canvas) {
+        this.driverManager = driverManager;
+        this.canvas = canvas;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        logger.info("Testing Visitor for CommandVisitorCanvas");
+
+        DriverCommand command = CommandsFeature.getDriverCommandManager().getCurrentCommand();
+        if(command==null){
+            logger.info("No command loaded");
+        }else {
+            CommandCanvasVisitor visitor = new CommandCanvasVisitor(canvas);
+
+            command.accept(visitor);
+
+            if(visitor.checkIfAllPointsOnCanvas())
+                logger.info("Boundary is crossed");
+            else
+                logger.info("Boundary is not crossed");
+        }
+    }
+}
